9c00a42bbcc70917a423e80efefe16d9f71b6ac0
Javid <jooshesh.javid@gmail.com>
2023-10-09 17:58:16 +0200

    Adds delete row

--- main.py
+++ main.py
@@ -10,10 +10,12 @@ import webbrowser
 import requests
 
 URL = "https://robotlab-residualwood.onrender.com/"
+URL_DEV = "http://localhost:5000/"
 KEYS = ["length", "width", "height", "weight", "density", "wood_species", "label", "color", "storage_location",
         "wood_id", "paint", "type",
         "project_type", "is_fire_treated", "is_straight", "is_planed", "image", "source", "info"]
 values = []
+param = []
 
 
 class BannerMsg:
@@ -76,7 +78,7 @@ class BannerMsg:
         self.page.update()
 
 
-def handle_request():
+def handle_post_request():
     inserted_data = list(zip(KEYS, values))
     payload = {}
     endpoint = "/residual_wood"
@@ -91,8 +93,19 @@ def handle_request():
     payload["timestamp"] = datetime.strftime(datetime.now(), "%d-%m-%Y %H:%M:%S")
     json_body = json.dumps(payload)
 
-    response = requests.post(URL + endpoint, data=json_body, timeout=5, headers=headers)
+    response = requests.post(URL_DEV + endpoint, data=json_body, timeout=10, headers=headers)
     # print(">>>>>>", json_body)
+    return response.json()
+
+
+def handle_delete_request():
+    payload = {}
+    if param:
+        for p in param:
+            payload["wood_id"] = p
+
+    json_body = json.dumps(payload)
+    response = requests.delete(URL, data=json_body, headers={"Content-Type": "application/json"})
     return response.json()
 
 
@@ -105,11 +118,69 @@ def main(page: Page):
     page.theme = flet.Theme(visual_density=flet.ThemeVisualDensity.COMFORTABLE, use_material3=True,
                             color_scheme_seed="#4336f5")
 
+    def tab_changed(e):
+        update_tab()
+
+    def submit(e):
+        error_status = False
+        if len(values) > 0:
+            values.clear()
+        for f in fields:
+            if (
+                    f.label == "Length *" and f.value == "" or
+                    f.label == "Width *" and f.value == "" or
+                    f.label == "Height *" and f.value == "" or
+                    f.label == "Weight *" and f.value == "" or
+                    f.label == "Density *" and f.value == "" or
+                    f.label == "Color *" and f.value == "" or
+                    f.label == "Source *" and f.value == ""
+            ):
+                error_msg(e)
+                error_status = True
+                break
+            values.append(f.value)
+        if error_status is False:
+            handle_post_request()
+            BannerMsg(page, "Successfully saved to DB", 'message', e)
+            time.sleep(1)
+            for f in fields:
+                if f != wood_id_text_field:
+                    f.value = ""
+            page.update()
+
+    def delete_row(e):
+        err = False
+        if len(param) > 0:
+            param.clear()
+        for f in fields:
+            if f.label == "Wood ID *" and f.value == "":
+                error_msg(e)
+                err = True
+                break
+            elif f == wood_id_text_field:
+                param.append(f.value)
+                break
+        if err is False:
+            handle_delete_request()
+            BannerMsg(page, f"Successfully deleted the row with Wood ID: {param[-1]}", 'message', e)
+            time.sleep(1)
+            for f in fields:
+                if f == wood_id_text_field:
+                    f.value = ""
+                    page.update()
+                    break
+
     def go_to_api_docs(e):
         webbrowser.open("https://robotlab-residualwood.onrender.com/swagger-ui")
 
     def go_to_github_page(e):
         webbrowser.open("https://github.com/Flabelatus/CircularWood_4.0_WP1")
+
+    tabs = flet.Tabs(
+        selected_index=0,
+        tabs=[flet.Tab(text="Insert Row"), flet.Tab(text="Delete Row")],
+        on_change=tab_changed
+    )
 
     length_text_field = TextField(label="Length *", tooltip="Length of the wood in mm", width=200, height=40,
                                   border_color="#4336f5", color="#4336f5")
@@ -131,7 +202,7 @@ def main(page: Page):
     storage_location_text_field = TextField(label="Storage Location",
                                             tooltip="A reference to where the wood is stored in the physical storage",
                                             width=200, height=40, border_color="#4336f5", color="#4336f5")
-    wood_id_text_field = TextField(label="Wood ID", tooltip="ID of the wood e.g. in the 7 digits format of '00000001'",
+    wood_id_text_field = TextField(label="Wood ID *", tooltip="ID of the wood e.g. in the 7 digits format of '00000001'",
                                    width=200, height=40, border_color="#4336f5", color="#4336f5")
     paint_text_field = TextField(label="RAL number", width=120, height=40, tooltip="The RAL number of the"
                                                                                    " wood if it is painted",
@@ -148,11 +219,16 @@ def main(page: Page):
     is_straight_checkbox = Checkbox(label="Straight", value=True)
     is_planned_checkbox = Checkbox(label="Planed", value=True)
 
-    image_path_field = TextField(label="Image path", tooltip="Path to where image is stored", width=200, height=40,
+    image_path_field = TextField(label="Image path", tooltip="Path to where image is stored", width=412, height=40,
                                  border_color="#4336f5", color="#4336f5")
     source_path_field = TextField(label="Source *", tooltip="Source location where the wood was collected", width=200,
                                   height=40, border_color="#4336f5", color="#4336f5")
     info_path_field = TextField(width=410, label="Info", multiline=True, border_color="#4336f5", color="#4336f5")
+
+    insert_btn = ElevatedButton(text="Insert", on_click=submit, bgcolor="#4336f5", color="white")
+    delete_btn = ElevatedButton(text="Delete", on_click=delete_row, bgcolor="#4336f5", color="white")
+
+    action_buttons = [insert_btn, delete_btn]
 
     fields = [
         length_text_field,
@@ -182,34 +258,31 @@ def main(page: Page):
         )
         BannerMsg(page, "Please fill in the required fields", "warning", e)
 
+    def update_tab():
+        status = tabs.tabs[tabs.selected_index].text
+        for f in fields:
+            if f != wood_id_text_field:
+                f.visible = (
+                    status == "Insert Row"
+                )
+            page.update()
+
+        for b in action_buttons:
+            if b == insert_btn:
+                b.visible = status == "Insert Row"
+            else:
+                b.visible = status == "Delete Row"
+            page.update()
+
+        for f in fields:
+            if f == wood_id_text_field:
+                f.visible = status == "Delete Row"
+                page.update()
+
     def cancel(e):
         for f in fields:
             f.value = None
         page.update()
-
-    def submit(e):
-        error_status = False
-        if len(values) > 0:
-            values.clear()
-
-        for f in fields:
-            if (
-                    f.label == "Length *" and f.value == "" or
-                    f.label == "Width *" and f.value == "" or
-                    f.label == "Height *" and f.value == "" or
-                    f.label == "Weight *" and f.value == "" or
-                    f.label == "Density *" and f.value == "" or
-                    f.label == "Color *" and f.value == "" or
-                    f.label == "Source *" and f.value == ""
-            ):
-                error_msg(e)
-                error_status = True
-                break
-            values.append(f.value)
-
-        if error_status is False:
-            resp = handle_request()
-            BannerMsg(page, "Successfully saved to DB", 'message', e)
 
     gui = Container(
         bgcolor="white",
@@ -220,6 +293,7 @@ def main(page: Page):
             controls=[
                 Divider(height=25, color="white"),
                 Card(color="#4336f5", content=Container(padding=10, content=Column(controls=[
+                    Image(src="RL LOGO WHITE .png", fit=flet.ImageFit.CONTAIN, width=200),
                     Text("DATA ENTRY GUI", size=22, weight=flet.FontWeight.W_900, color="white"),
                     Text("contact: j.jooshesh@hva.nl", size=14,
                          weight=flet.FontWeight.W_200, selectable=True, color="white"),
@@ -232,16 +306,17 @@ def main(page: Page):
                                                  color="#4336f5")]),
 
                 ], horizontal_alignment=flet.CrossAxisAlignment.CENTER, wrap=True))),
+                tabs,
 
                 Divider(height=25, color="white"),
 
                 Row(controls=[
                     Column(controls=[length_text_field, width_text_field, height_text_field, weight_text_field,
-                                     wood_id_text_field]),
+                                     wood_id_text_field, source_path_field]),
                     Column(controls=[density_text_field, wood_species_field, label_field, color_text_field,
                                      storage_location_text_field])],
                     alignment=flet.MainAxisAlignment.CENTER),
-                Row(controls=[image_path_field, source_path_field], alignment=flet.MainAxisAlignment.CENTER),
+                Row(controls=[image_path_field], alignment=flet.MainAxisAlignment.CENTER),
                 Divider(height=25, visible=True, color="white"),
                 Row(controls=[is_planned_checkbox, is_straight_checkbox, is_fire_treated_checkbox],
                     alignment=flet.MainAxisAlignment.CENTER),
@@ -255,7 +330,7 @@ def main(page: Page):
                 Divider(height=30, visible=True, color="white"),
                 Row(controls=[
                     Column(
-                        controls=[ElevatedButton(text="Save", on_click=submit, bgcolor="#4336f5", color="white")]),
+                        controls=action_buttons),
                     Column(controls=[ElevatedButton(text="Clear", on_click=cancel, bgcolor="grey", color="white")]),
                     Column(controls=[
                         ElevatedButton(text="Close", on_click=lambda x: page.window_destroy(), bgcolor="grey",
@@ -267,7 +342,7 @@ def main(page: Page):
     page.views.append(
         gui
     )
-    page.update()
+    update_tab()
 
 
 flet.app(target=main)
